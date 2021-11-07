aba009a0548eaa860f0538fbde9281cb6d814ea0
wsykurski <wsykurski@gmail.com>
2021-11-07 12:19:25 +0100

    WiP

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// jest.config.js
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue'
+  ],
+  transform: {
+    '^.+\\.vue$': 'vue-jest'
+  }
+}
---
+++ src/composables/useRockets.test.js
@@ -0,0 +1,6 @@
+import { mount } from '@vue/test-utils';
+import useRockets from './useRockets';
+
+describe('Mount it', () => {
+  const wrapper = mount(useRockets, {})
+})
